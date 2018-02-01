594d1780891f2f31f8cf1ffdfeca3baec10fc2cb
Travis Roemhild <travis.roemhild@gmail.com>
2018-01-31 20:21:34 -0700

    app.get updated

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/view", function(req, res) {
+app.get("/make", function(req, res) {
   res.sendFile(path.join(__dirname, "make.html"));
 });
 
@@ -35,6 +35,20 @@ app.get("/view", function(req, res) {
 app.get("/api", function(req, res) {
   res.json(reservations);
 });
+
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newRes = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  console.log(newRes);
+
+  reservations.push(newRes);
+
+  res.json(newRes);
+});
+
 
 app.listen(port, function() {
   console.log("App listening on PORT " + port);
